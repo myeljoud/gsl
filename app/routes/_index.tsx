d4c0b7599c202a7a73f7d70d5d2077a13d422f79
Mohamed Yahye El Joud <yahyajoud@gmail.com>
2024-11-02 14:25:10 +0000

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -58,7 +58,7 @@ export default function Index() {
         />
 
         <div className="grid grid-cols-1 tablet:grid-cols-2 gap-4 relative z-20">
-          <div className="order-2 tablet:order-1 flex flex-col items-start gap-4 py-2">
+          <div className="order-2 tablet:order-1 flex flex-col items-start gap-4 py-2 px-4 tablet:px-0">
             <h1 className="font-robotoCondensed text-38 leading-38 tablet:text-6xl font-extrabold text-center tablet:text-start">
               {heroSection.header}
             </h1>
